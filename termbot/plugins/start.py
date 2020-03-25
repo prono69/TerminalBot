b02d8a31f4068f7201bc1eb2e1fea64f71fc2e22
Shahnawaz Alam <55076817+prono69@users.noreply.github.com>
2020-03-25 11:30:19 +0530

    Add files via upload

---
+++ termbot/plugins/start.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Telegram Terminal Bot
+# CopyLeft AGPLv3 (C) 2020 The Authors
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from pyrogram import Client, Filters
+
+from termbot import (
+    AUTH_USERS,
+    HELP_STICKER
+)
+
+
+@Client.on_message(~Filters.chat(chats=AUTH_USERS))
+async def not_auth_text(client, message):
+    await message.reply("`Hey I am Online. Now Fu#k Off. \n Nikal Lawde`🐈")
